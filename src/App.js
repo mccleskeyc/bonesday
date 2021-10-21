f407c6d4d46126a65ca0b73e3e5ad866e5e961b3
mccleskeyc <mccleskeyc@gmail.com>
2021-10-21 11:56:36 -0400

    basic css

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
   render() {
   return (
     <div className="App">
-      <h1>Is It a Bones Day?</h1>
+      <h1>Is Today A Bones Day?</h1>
       
       <Bones />
       ---
