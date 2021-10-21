b3e692a8fba9b1bcfd9efddda10343742fd95a3b
mccleskeyc <mccleskeyc@gmail.com>
2021-10-21 11:38:07 -0400

    bones formatting issues addressed

--- src/components/Bones.js
+++ src/components/Bones.js
@@ -5,14 +5,23 @@ export class Bones extends Component {
         return (
             <div className="decisioncontainer">
                 <div className="intro">
-                Noodle says today is a...
+                    Noodle says today is a...
                 </div>
+                
+                <br />
+
                 <div className="decisiontext">Bones Day!</div>
-                <img 
-                className="bonesimg"
-                src="https://media.npr.org/assets/img/2021/10/19/screen-shot-2021-10-19-at-11.51.31_custom-e5ca476577cde7ec020eebd95b7daa4228cfe9af-s1200-c85.webp"
-                alt="Screenshot from @jongraz's TikTok showing Noodle with bones"></img>
+
+                <img
+                    className="bonesimg"
+                    src="https://media.npr.org/assets/img/2021/10/19/screen-shot-2021-10-19-at-11.51.31_custom-e5ca476577cde7ec020eebd95b7daa4228cfe9af-s1200-c85.webp"
+                    alt="Screenshot from @jongraz's TikTok showing Noodle with bones"></img>
+                    
+                <br />
+                <br />
+
                 <div className="decisioninfo">He's feeling jazzed! Go ahead and treat yourself, take that leap of faith, and have a good day!</div>
+            
             </div>
         )
     }
