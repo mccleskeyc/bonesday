badde5170bb12a158b773e7fd1e8c6afb2ad080d
mccleskeyc <mccleskeyc@gmail.com>
2021-10-21 11:44:21 -0400

    no bones skeleton

--- src/components/NoBones.js
+++ src/components/NoBones.js
@@ -3,8 +3,25 @@ import React, { Component } from 'react'
 export class NoBones extends Component {
     render() {
         return (
-            <div>
-                No Bones Day
+            <div className="decisioncontainer">
+                <div className="intro">
+                    Noodle says today is a...
+                </div>
+                
+                <br />
+
+                <div className="decisiontext">No Bones Day!</div>
+
+                <img
+                    className="bonesimg"
+                    src="https://i.imgur.com/Mb3hL5b.jpg"
+                    alt="Screenshot from @jongraz's TikTok showing Noodle without bones"></img>
+                    
+                <br />
+                <br />
+
+                <div className="decisioninfo">Noodle is giving you permission to take the time you need to listen to your body. Slow things down today, practice self care, and maybe call out of work.</div>
+            
             </div>
         )
     }
